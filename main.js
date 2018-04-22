7fd30a4b41df035ddc717d05375ea7d134180cc5
Toufiq <burningraven06@gmail.com>
2018-04-22 23:39:15 +0600

    create sortable contacts with header

---
+++ main.js
@@ -0,0 +1,124 @@
+document.getElementById("saveContactBtn").addEventListener("click", addContact);
+
+let filterInput = document.getElementById('filterInput');
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames(){
+	filterInputValue = filterInput.value;
+	let ulElem = document.getElementById('names');
+	let liElem = ul.querySelectorAll('li.collection-item');
+
+}
+
+function checkAndCreateLocalStorageData(){
+	if (localStorage.getItem("contacts") === null){
+		var theContacts = [
+			{"name": "Barry", 	"cell": "127343"},
+			{"name": "Andy", 		"cell": "123123"},
+			{"name": "Cinnamon","cell": "123145"},
+			{"name": "Abraham", "cell": "123346"},
+			{"name": "Cindy", 	"cell": "123168"},
+			{"name": "Apollo", 	"cell": "143523"},
+			{"name": "Ben", 		"cell": "126903"},
+			{"name": "Arnold", 	"cell": "124253"},
+			{"name": "Bud", 		"cell": "134063"},
+			{"name": "Colin", 	"cell": "123491"},
+			{"name": "Bill", 		"cell": "157323"},
+			{"name": "Adam", "cell": "123194"},
+			{"name": "Cedrick", "cell": "123194"},
+			{"name": "Alex", "cell": "123194"},
+			{"name": "Bulan", "cell": "123194"},
+			{"name": "Atom", "cell": "123194"},
+
+		];
+
+		theContacts = theContacts.sort( (a,b) => {
+			if (a.name < b.name) return -1;
+			if (a.name > b.name) return 1;
+			return 0;
+		});
+
+		theContacts = JSON.stringify(theContacts);
+		localStorage.setItem("contacts", theContacts);
+	}
+}
+
+function fetchLocalStorageData(){
+
+	checkAndCreateLocalStorageData();
+
+	var data = localStorage.getItem("contacts");
+	data = JSON.parse(data);
+	data.sort( (a,b) => {
+			if (a.name < b.name) return -1;
+			if (a.name > b.name) return 1;
+			return 0;
+		});
+
+	return data;
+}
+
+function populateDiv(){
+
+	lsContactsData = fetchLocalStorageData();
+
+	outputDiv = document.getElementById('contactsDiv');
+	childUL = document.getElementById("lsContactNames");
+	childUL.innerHTML = "";
+
+	var newCharLiElem = document.createElement("li");
+	var strContent  = `${lsContactsData[0].name.charAt(0)}`
+// 	var elemContent = document.createTextNode(strContent);
+// 	newCharLiElem.appendChild(elemContent);
+	newCharLiElem.innerHTML = `<h5>${strContent}</h5>`;
+	newCharLiElem.setAttribute("class", 'collection-header');
+	childUL.appendChild(newCharLiElem);
+
+
+	for (var i=0; i<lsContactsData.length; i++){
+
+		liElemStr = `${lsContactsData[i].name} - ${lsContactsData[i].cell}`;
+
+		var newContactLiElem = document.createElement("li");
+		var liElemContent = document.createTextNode(liElemStr);
+		newContactLiElem.appendChild(liElemContent);
+		newContactLiElem.setAttribute("class", "collection-item");
+		childUL.appendChild(newContactLiElem);
+
+
+		firstNameChar = lsContactsData[i].name.charAt(0);
+		nextNameChar = lsContactsData[i+1].name.charAt(0);
+
+		if (firstNameChar != nextNameChar){
+
+			var newCharLiElem = document.createElement("li");
+			var strContent  = `<h5>${nextNameChar}</h5>`;
+			newCharLiElem.innerHTML = strContent;
+			newCharLiElem.setAttribute("class", 'collection-header');
+			childUL.appendChild(newCharLiElem);
+		}
+
+	}
+}
+
+function addContact(e){
+	e.preventDefault();
+	var contactName= document.getElementById('createForm').newContactName.value;
+	var contactCell= document.getElementById('createForm').newContactCell.value;
+
+	if (!contactName || !contactCell){
+		document.getElementById('createFormErrors').innerHTML = "<p class='center-align'> Please Fill in values correctly </p>";
+	return ;
+	}
+
+	var newContact = { name : contactName, cell : contactCell};
+
+	currentContacts = JSON.parse(localStorage.getItem("contacts"));
+	currentContacts.push(newContact);
+	currentContacts = JSON.stringify(currentContacts);
+	localStorage.setItem("contacts", currentContacts);
+	contactName.value = "";
+	contactCell.value = "";
+	populateDiv();
+
+}
